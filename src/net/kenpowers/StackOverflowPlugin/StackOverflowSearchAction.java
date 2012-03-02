33b9847aec8e604b0d8354598518b6d66a642793
Kenneth Powers <mail@kenpowers.net>
2012-03-02 13:19:19 -0500

    Implemented getWordAtCaret

--- src/net/kenpowers/StackOverflowPlugin/StackOverflowSearchAction.java
+++ src/net/kenpowers/StackOverflowPlugin/StackOverflowSearchAction.java
@@ -12,4 +12,37 @@ public class StackOverflowSearchAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
     }
+
+    /**
+     * Gets the word from the editor where the caret is.
+     *
+     * @param editorText  The text from the editor.
+     * @param caretOffset Where teh caret is in the editor.
+     * @return The word where the caret is in the editor.
+     */
+    private String getWordAtCaret(CharSequence editorText, int caretOffset) {
+        if (editorText.length() == 0) return null;
+
+        if (caretOffset > 0 && !Character.isJavaIdentifierPart(editorText.charAt(caretOffset))
+            && Character.isJavaIdentifierPart(editorText.charAt(caretOffset - 1))) {
+            caretOffset--;
+        }
+
+        if (Character.isJavaIdentifierPart(editorText.charAt(caretOffset))) {
+            int start = caretOffset;
+            int end = caretOffset;
+
+            while (start > 0 && Character.isJavaIdentifierPart(editorText.charAt(start - 1))) {
+                start--;
+            }
+
+            while (end < editorText.length() && Character.isJavaIdentifierPart(editorText.charAt(end))) {
+                end++;
+            }
+
+            return editorText.subSequence(start, end).toString();
+        }
+
+        return null;
+    }
 }
