d34b7bb1eab91e5875d0efa064b46ab72bcb115a
Kenneth Powers <mail@kenpowers.net>
2012-03-02 13:05:18 -0500

    Created StackOverflowSearchAction

---
+++ src/net/kenpowers/StackOverflowPlugin/StackOverflowSearchAction.java
@@ -0,0 +1,15 @@
+package net.kenpowers.StackOverflowPlugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Action class for searching StackOverflow from the tools menu.
+ *
+ * @author Kenneth Powers
+ */
+public class StackOverflowSearchAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
